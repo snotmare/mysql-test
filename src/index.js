1fdbd33573f83579492afdd4818cd5dead5bddc2
Benjamin Ferguson <ben.ferguson@duncanaviation.com>
2023-10-02 07:49:43 -0500

    It worked

---
+++ src/index.js
@@ -0,0 +1,2 @@
+let name = 'Ben';
+console.log(name);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import { createConnection } from 'mysql2';
+
 console.log('It worked');
+
+// create the connection to database
+const connection = createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'world'
+});
+
+async function runQuery() {
+	let results = await connection.promise().query('SELECT count(*) FROM city');
+	console.log(results);
+}
+
+runQuery();
